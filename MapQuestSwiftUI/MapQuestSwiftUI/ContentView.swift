c52a7fb5111c882854d5e277c824df6953b08a21
puma <cwenyu2424@gmail.com>
2024-11-19 14:47:03 +0800

    [feat] Create MapQuestSwiftUI Project

---
+++ MapQuestSwiftUI/MapQuestSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapQuestSwiftUI
+//
+//  Created by 莊雯聿 on 2024/11/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapQuestSwiftUI/MapQuestSwiftUI/MapQuestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapQuestSwiftUIApp.swift
+//  MapQuestSwiftUI
+//
+//  Created by 莊雯聿 on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct MapQuestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
